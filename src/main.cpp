5687e483fc05b8ba910e1122fe196739c90ac953
CatsaCode <catsacode@gmail.com>
2025-04-19 13:06:55 -0400

    Overwrite all text to "AnyText"
    Every piece of text in the entire game is now replaced with the string "AnyText"
    This proves the concept of the mod.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,15 @@
 
 #include "scotland2/shared/modloader.h"
 
+// #include "GlobalNamespace/StandardLevelDetailView.hpp"
+// #include "GlobalNamespace/StandardLevelDetailViewController.hpp"
+// #include "UnityEngine/UI/Button.hpp"
+// #include "UnityEngine/GameObject.hpp"
+// #include "HMUI/CurvedTextMeshPro.hpp"
+
+#include "TMPro/TextMeshPro.hpp"
+#include "TMPro/TextMeshProUGUI.hpp"
+
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
 // the modloader upon startup
@@ -10,27 +19,54 @@ static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // other config tools such as config-utils don't use this config, so it can be
 // removed if those are in use
 Configuration &getConfig() {
-  static Configuration config(modInfo);
-  return config;
+    static Configuration config(modInfo);
+    return config;
 }
+
+
+
+MAKE_HOOK_MATCH(TextMeshProHook, &TMPro::TextMeshPro::SetVerticesDirty, void, 
+TMPro::TextMeshPro* self) {
+    
+    // Used for hit scores
+    self->set_richText(true);
+    self->set_text("<color=#00ff00>AnyText");
+
+    TextMeshProHook(self);
+}
+
+MAKE_HOOK_MATCH(TextMeshProUGUIHook, &TMPro::TextMeshProUGUI::SetVerticesDirty, void, 
+TMPro::TextMeshProUGUI* self) {
+    
+    // Used for everything else
+    self->set_richText(true);
+    self->set_text("<size=0>Hehe secret text :3</size>AnyText");
+
+    TextMeshProUGUIHook(self);
+}
+
+
 
 // Called at the early stages of game loading
 MOD_EXTERN_FUNC void setup(CModInfo *info) noexcept {
-  *info = modInfo.to_c();
+    *info = modInfo.to_c();
 
-  getConfig().Load();
+    getConfig().Load();
 
-  // File logging
-  Paper::Logger::RegisterFileContextId(PaperLogger.tag);
+    // File logging
+    Paper::Logger::RegisterFileContextId(PaperLogger.tag);
 
-  PaperLogger.info("Completed setup!");
+    PaperLogger.info("Completed setup!");
 }
 
 // Called later on in the game loading - a good time to install function hooks
 MOD_EXTERN_FUNC void late_load() noexcept {
-  il2cpp_functions::Init();
+    il2cpp_functions::Init();
 
-  PaperLogger.info("Installing hooks...");
+    PaperLogger.info("Installing hooks...");
 
-  PaperLogger.info("Installed all hooks!");
+    INSTALL_HOOK(PaperLogger, TextMeshProHook);
+    INSTALL_HOOK(PaperLogger, TextMeshProUGUIHook)
+
+    PaperLogger.info("Installed all hooks!");
 }
