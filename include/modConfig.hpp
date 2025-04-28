a5c4f7abb0d3829b550b18f52b06a0714dad648b
CatsaCode <catsacode@gmail.com>
2025-04-27 23:19:21 -0400

    Update modConfig
    Added curly brackets to modConfig.hpp
    to adjust for the syntax in the new version and fix build errors.

--- include/modConfig.hpp
+++ include/modConfig.hpp
@@ -2,7 +2,7 @@
 
 #include "config-utils/shared/config-utils.hpp"
 
-DECLARE_CONFIG(ModConfig,
+DECLARE_CONFIG(ModConfig) {
     CONFIG_VALUE(findStrings, std::vector<std::string>, "findStrings", {"ANYTEXT"});
     CONFIG_VALUE(replaceStrings, std::vector<std::string>, "replaceStrings", {"<color=green>ANYTEXT"});
-)
+};
