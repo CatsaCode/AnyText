f241344fa3371546cbdf47cf74b60a0c0267725c
CatsaCode <catsacode@gmail.com>
2025-08-22 14:21:18 -0400

    Fix mod config for 1.37.0
    config-utils uses a different syntax in 1.37.0, so an ifdef was added in modConfig.hpp to account for it.

--- include/modConfig.hpp
+++ include/modConfig.hpp
@@ -2,7 +2,17 @@
 
 #include "config-utils/shared/config-utils.hpp"
 
+#ifdef BS_1_37_0
+DECLARE_CONFIG(ModConfig,
+#else
 DECLARE_CONFIG(ModConfig) {
+#endif
+
     CONFIG_VALUE(findStrings, std::vector<std::string>, "findStrings", {"ANYTEXT"});
     CONFIG_VALUE(replaceStrings, std::vector<std::string>, "replaceStrings", {"<color=green>ANYTEXT"});
+
+#ifdef BS_1_37_0
+);
+#else
 };
+#endif
