31fdfbdb612bfd97b72926e852ed95b411d4157b
CatsaCode <catsacode@gmail.com>
2025-08-25 15:30:15 -0400

    Move FindReplaceEntry to configs.hpp
    Separate the config files and the mod config.
    More functionality needs to be added that are specific to the individual configs.

--- include/modConfig.hpp
+++ include/modConfig.hpp
@@ -1,48 +1,6 @@
 #pragma once
 
 #include "config-utils/shared/config-utils.hpp"
-#include "UnityEngine/Color.hpp"
-
-namespace AnyText {
-
-    // enum class FindAlgorithm {
-    //     ExactMatch,
-    //     PartialMatch,
-    //     Regex
-    // };
-
-    // enum class FindLocation {
-    //     MenuScene,
-    //     GameScene
-    // };
-
-    DECLARE_JSON_STRUCT(FindReplaceEntry) {
-        VALUE(std::string, findString);
-        VALUE(std::string, replaceString);
-
-        // VALUE_DEFAULT(FindAlgorithm, findAlgorithm, FindAlgorithm::PartialMatch);
-        // VALUE_DEFAULT(bool, accumulate, false);
-        // VALUE_OPTIONAL(FindLocation, findLocation);
-
-        // VALUE_OPTIONAL(bool, replaceAll);
-        // VALUE_OPTIONAL(bool, bold);
-        // VALUE_OPTIONAL(bool, italics);
-        // VALUE_OPTIONAL(bool, underline);
-        // VALUE_OPTIONAL(bool, strikethrough);
-        // VALUE_OPTIONAL(bool, lowercase);
-        // VALUE_OPTIONAL(bool, uppercase);
-        // VALUE_OPTIONAL(bool, smallcaps);
-        // VALUE_OPTIONAL(int, fontSize);
-        // VALUE_OPTIONAL(bool, autoSize);
-        // Color
-        // Gradient
-        // Spacing options
-        // Alignment
-        // Wrapping
-        // Overflow
-    };
-
-}
 
 #ifdef BS_1_37_0
 DECLARE_CONFIG(ModConfig,
