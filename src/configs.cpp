2e21d543644cb3485b363f60d8cf9eeeb2784629
CatsaCode <catsacode@gmail.com>
2025-08-25 17:42:35 -0400

    Load config files into configs vector

--- src/configs.cpp
+++ src/configs.cpp
@@ -5,9 +5,34 @@
 
 using namespace AnyText;
 
-std::vector<Config> AnyText::configs;
+namespace AnyText {
 
-void AnyText::loadConfigs() {
-    PaperLogger.info("Fetching configs from path: '{}'", getAnyTextDir());
-    std::filesystem::recursive_directory_iterator(getAnyTextDir());
+    std::vector<Config> configs;
+    
+    void loadConfigs() {
+        configs.clear();
+
+        for(auto& file : std::filesystem::recursive_directory_iterator(getAnyTextDir())) {
+            if(file.path().extension() != ".json") continue;
+
+            std::string filePath = file.path();
+            PaperLogger.info("Loading config from path: '{}'", filePath);
+            Config config;
+            try {
+                ReadFromFile(filePath, config);
+            } catch(const std::exception& err) {
+                PaperLogger.error("Failed to load config '{}', Error: {}", filePath, err.what());
+                continue;
+            }
+
+            configs.push_back(config);
+
+            // DEBUG
+            PaperLogger.info("Version: {}", config.version);
+            for(FindReplaceEntry& entry : config.entries) {
+                PaperLogger.info("Find: '{}', Replace: '{}'", entry.findString, entry.replaceString);
+            }
+        }
+    }
+
 }
