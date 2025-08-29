ca77ad001c333a5d90325b53f50c2baee0d4e316
CatsaCode <catsacode@gmail.com>
2025-08-28 21:26:15 -0400

    Identify text before continuing the ParseInputText hook
    Let AnyText modify the text before the ParseInputText method runs, rather than after.
    This helps ensure that the text state gets updated properly and make sure the layout is calculated correctly.

--- src/textIdentifier.cpp
+++ src/textIdentifier.cpp
@@ -10,26 +10,22 @@ using namespace UnityEngine;
 
 namespace AnyText {
 
-    MAKE_HOOK_MATCH(ParseInputText_IdentifyText, &TMPro::TMP_Text::ParseInputText, void,
-        TMPro::TMP_Text* self
-    ) {
-        ParseInputText_IdentifyText(self);
+    void identifyText(TMPro::TMP_Text* text) {
+        if(!text || !text->get_text() || !text->get_gameObject()) return;
 
-        if(!self || !self->get_text() || !self->get_gameObject()) return;
-
-        Transform* menuTransform = self->get_transform();
+        Transform* menuTransform = text->get_transform();
         for(int i = 0; i < 4; i++) {
             if(!menuTransform) break;
             if(menuTransform->get_name() == "AnyTextMenu") return;
             menuTransform = menuTransform->get_parent();
         }
 
-        PaperLogger.info("ParseInputText_IdentifyText has valid TMP_Text: '{}'", self->get_text());
+        PaperLogger.info("identifyText has valid TMP_Text: '{}'", text->get_text());
         
-        TextManager* textManager = self->GetComponent<TextManager*>();
+        TextManager* textManager = text->GetComponent<TextManager*>();
         if(textManager && textManager->isApplyingState) {PaperLogger.info("TextManager is already replacing text, aborting"); return;}
 
-        std::string originalText = textManager ? textManager->originalState.text : std::string(self->get_text());
+        std::string originalText = textManager ? textManager->originalState.text : std::string(text->get_text());
 
         for(Config& config : configs) {
             for(FindReplaceEntry& entry : config.entries) {
@@ -38,7 +34,7 @@ namespace AnyText {
                 if(static_cast<FindAlgorithm>(entry.findAlgorithm) == FindAlgorithm::ExactMatch && (!tmpFound || originalText.size() != entry.findString.size())) continue;
                 PaperLogger.info("Original text '{}' matched entry with findAlgorithm: {}, findString: '{}'", originalText, entry.findAlgorithm, entry.findString);
 
-                if(!textManager) {PaperLogger.info("TextManager does not exist, adding component"); self->get_gameObject()->AddComponent<TextManager*>();}
+                if(!textManager) {PaperLogger.info("TextManager does not exist, adding component"); text->get_gameObject()->AddComponent<TextManager*>();}
                 else {
                     PaperLogger.info("TextManager already exists, calling OnChangeText");
                     textManager->OnTextChange();
@@ -49,6 +45,13 @@ namespace AnyText {
         }
 
         if(textManager) Object::Destroy(textManager);
+    }
+
+    MAKE_HOOK_MATCH(ParseInputText_IdentifyText, &TMPro::TMP_Text::ParseInputText, void,
+        TMPro::TMP_Text* self
+    ) {
+        identifyText(self);
+        ParseInputText_IdentifyText(self);
     }
 
     void installTextIdentifierHooks() {
