f8b0f53d6bef9e2c51c8ea711e9d61c0e3fa3989
CatsaCode <catsacode@gmail.com>
2025-09-07 21:34:15 -0400

    Protoype new UI with createConfigSetting
    Start writing new UI by generating each list element with a function.
    This is what I did before, but I'm starting to think it may not be the best approach. I want to evaluate the configs when the AnyTextViewController back button is pressed rather than upon immidiate updating of any UI, which means more state and UI complexity. It'd be nice if each config setting had a component, and HMUI::TableView and TableCell is starting to look more appealing the more I look at it.

--- include/ui/configsViewController.hpp
+++ include/ui/configsViewController.hpp
@@ -2,8 +2,11 @@
 
 #include "custom-types/shared/macros.hpp"
 
+#include "UnityEngine/RectTransform.hpp"
 #include "HMUI/ViewController.hpp"
 
 DECLARE_CLASS_CODEGEN(AnyText::UI, ConfigsViewController, HMUI::ViewController) {
+    DECLARE_INSTANCE_FIELD(UnityW<UnityEngine::RectTransform>, configsVerticalTransform);
+
     DECLARE_OVERRIDE_METHOD_MATCH(void, DidActivate, &HMUI::ViewController::DidActivate, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
 };
