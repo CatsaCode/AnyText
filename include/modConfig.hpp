77938cb75ebaebc318d9ee7c3c13db416bb7b430
CatsaCode <catsacode@gmail.com>
2025-10-09 12:33:20 -0400

    Upgrade v1.0.1 configs to v1.1.0
    Added a DESERIALIZE_FUNCTION to modConfig that will convert the old findStrings and replaceStrings vectors and turn them into a new Config called "UpgradedConfig".
    Some people have actually found a purpose for this mod, and others have put a lot of time into creating their config ( Looking at you, Ven :3 ). Would not be particuarly great if the update forced a reset.

--- include/modConfig.hpp
+++ include/modConfig.hpp
@@ -1,21 +1,24 @@
 #pragma once
 
+#include "main.hpp"
 #include "config-utils/shared/config-utils.hpp"
 
-#ifdef BS_1_37_0
-DECLARE_CONFIG(ModConfig,
-#else
-DECLARE_CONFIG(ModConfig) {
-#endif
-    
-    CONFIG_VALUE(findStrings, std::vector<std::string>, "findStrings", {"ANYTEXT"});
-    CONFIG_VALUE(replaceStrings, std::vector<std::string>, "replaceStrings", {"<color=green>ANYTEXT"});
-    // KEEP_EXTRA_FIELDS;
+namespace AnyText {
 
-    CONFIG_VALUE(configOrder, std::vector<std::string>, "configOrder", {});
-    
-#ifdef BS_1_37_0
-);
-#else
-};
-#endif
+    #ifdef BS_1_37_0
+    DECLARE_CONFIG(ModConfig,
+    #else
+    DECLARE_CONFIG(ModConfig) {
+    #endif
+        CONFIG_VALUE(configOrder, std::vector<std::string>, "configOrder", {});
+        KEEP_EXTRA_FIELDS;
+        
+        DESERIALIZE_FUNCTION(upgradeVersion);
+        
+    #ifdef BS_1_37_0
+    );
+    #else
+    };
+    #endif
+
+}
---
+++ src/upgrade.cpp
@@ -0,0 +1,40 @@
+#include "modConfig.hpp"
+#include "main.hpp"
+
+#include "configs.hpp"
+
+#include <algorithm>
+
+namespace AnyText {
+
+    void ModConfig_t::upgradeVersion(rapidjson::Value& jsonValue) {
+        if(!jsonValue.HasMember("findStrings") || !jsonValue.HasMember("replaceStrings")) return;
+        rapidjson::Value& findStrings = jsonValue["findStrings"];
+        rapidjson::Value& replaceStrings = jsonValue["replaceStrings"];
+        if(!findStrings.IsArray() || !replaceStrings.IsArray()) return;
+    
+        PaperLogger.info("Attempting upgrade from v1.0.1 to v1.1.0 format");
+        Config& upgradedConfig = configs.emplace_back();
+        upgradedConfig.name = "FAILED_TO_UPGRADE_CONFIG";
+
+        if(findStrings.GetArray().Size() != replaceStrings.GetArray().Size()) PaperLogger.warn("findStrings and replaceStrings have a different size: {}-{}", findStrings.GetArray().Size(), replaceStrings.GetArray().Size());
+        int numEntries = std::min(findStrings.GetArray().Size(), replaceStrings.GetArray().Size());
+        PaperLogger.info("numEntries: {}", numEntries);
+
+        for(int i = 0; i < numEntries; i++) {
+            if(!findStrings.GetArray()[i].IsString() || !replaceStrings.GetArray()[i].IsString()) {PaperLogger.error("Unexpected data type"); return;}
+            FindReplaceEntry& entry = upgradedConfig.entries.emplace_back();
+            entry.findString = findStrings.GetArray()[i].GetString();
+            entry.replaceString = replaceStrings.GetArray()[i].GetString();
+            entry.findAlgorithm = static_cast<int>(FindAlgorithm::ExactMatch);
+        }
+
+        upgradedConfig.name = "UpgradedConfig";
+        upgradedConfig.unsaved = true;
+        saveConfig(upgradedConfig);
+        jsonValue.RemoveMember(findStrings);
+        jsonValue.RemoveMember(replaceStrings);
+    }
+
+
+}
